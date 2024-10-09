a69cc750020552f317df7027faafb8fa5c328b12
KrishnaananthamSamahithan <krishnanantham.20210863@iit.ac.lk>
2024-10-09 12:10:39 +0530

    finish login

--- src/common/index.js
+++ src/common/index.js
@@ -4,7 +4,16 @@ const SummaryApi = {
     signUp : {
         url : `${backendDomain}/api/signup`,
         method : "POST"
+    },
+    signIn : {
+        url : `${backendDomain}/api/signin`,
+        method : "POST"
+    },
+    current_user : {
+        url : `${backendDomain}/api/user-details`,
+        method : "GET"
     }
+
 }
 
 export default SummaryApi;
---
+++ src/common/role.js
@@ -0,0 +1,4 @@
+const ROLE = { 
+    Admin : "Admin",
+    GENERAL : "GENERAL"
+}
